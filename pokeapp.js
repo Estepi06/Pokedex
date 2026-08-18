f2da56e7932d17a454303fbc25d4314445eb1f9e
Estepi06 <jpineros@cesde.net>
2026-08-17 23:38:24 -0500

    Refactor weight and height calculations in pokeapp.js
    se mejoran las stats y se limpia el codigo

--- pokeapp.js
+++ pokeapp.js
@@ -1,9 +1,7 @@
-// Seleccionamos los elementos del HTML
 let divContenedor = document.querySelector("#contenedor");
 let btnBuscar = document.querySelector("#btnBuscar");
 let inputNombre = document.querySelector("#pokemonInput");
 
-// Evento al hacer clic en el botón Buscar
 btnBuscar.addEventListener("click", async () => {
     let nombreBuscado = inputNombre.value.trim();
 
@@ -11,21 +9,17 @@ btnBuscar.addEventListener("click", async () => {
         alert("Escribe el nombre de un Pokémon primero");
         return;
     }
-
-    // Llamamos a la función definida en pokeapi.js
     let pokemon = await obtenerDatosApi(nombreBuscado);
 
     if (pokemon) {
-        // Extraemos los datos que queremos mostrar
         let numero = pokemon.id;
         let nombre = pokemon.name;
         let imagen = pokemon.sprites.front_default;
         let tipo = pokemon.types[0].type.name;
-        let peso = pokemon.weight / 10; // Convertido a kg
-        let altura = pokemon.height / 10; // Convertido a metros
+        let peso = pokemon.weight / 10; 
+        let altura = pokemon.height / 10; 
         let habilidad = pokemon.abilities[0].ability.name;
 
-        // Armamos la tarjeta completa estilo Pokédex
         divContenedor.innerHTML = `
             <div class="tarjeta-pokedex">
                 <span class="numero">#${numero}</span>
