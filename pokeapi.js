87fcbbee7eed1db9123802f2cb7365867a0caec2
Estepi06 <jpineros@cesde.net>
2026-08-17 23:36:43 -0500

    Clean up
    Removed comments for clarity in the obtenerDatosApi function.

--- pokeapi.js
+++ pokeapi.js
@@ -1,15 +1,9 @@
 async function obtenerDatosApi(nombre) {
     try {
-        // Armamos la URL con el nombre que escriba el usuario
         let url = `https://pokeapi.co/api/v2/pokemon/${nombre.toLowerCase()}`;
-        
-        // Hacemos la petición
+        //petición
         let respuesta = await fetch(url);
-        
-        // Convertimos la respuesta a un objeto JSON
         let pokemon = await respuesta.json();
-        
-        // Devolvemos el resultado
         return pokemon;
 
     } catch (error) {
